0222b96af358c971dbf8b59c1dfb256517c1620b
Zhao Vincent <ravensore@gmail.com>
2012-08-08 14:45:06 +0800

    add c include path in macosx

--- torment-cpp.el
+++ torment-cpp.el
@@ -14,6 +14,18 @@
                                      "-IC:/MinGW/lib/gcc/mingw32/4.6.2/include"
                                      "-IC:/MinGW/lib/gcc/mingw32/4.6.2/include/c++" 
                                      "-D__MSVCRT__=")))
+(if (equal system-type 'windows-nt)
+	(setq ac-clang-flags
+		  (mapcar (lambda (item) (concat "-I" item)
+					(split-string "
+ /usr/llvm-gcc-4.2/bin/../lib/gcc/i686-apple-darwin11/4.2.1/include
+ /usr/include/c++/4.2.1
+ /usr/include/c++/4.2.1/backward
+ /usr/local/include
+ /Applications/Xcode.app/Contents/Developer/usr/llvm-gcc-4.2/lib/gcc/i686-apple-darwin11/4.2.1/include
+ /usr/include
+")))))
+
 
 (add-hook 'c-mode-common-hook
   (lambda() 
