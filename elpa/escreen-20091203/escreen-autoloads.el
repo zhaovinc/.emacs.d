712921cac608db0762ac488ded6d734fc1e0ddaf
Zhao Vincent <v.zhao@sap.com>
2012-08-15 17:15:46 +0800

    switch to elpa: step 2

---
+++ elpa/escreen-20091203/escreen-autoloads.el
@@ -0,0 +1,18 @@
+;;; escreen-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("escreen-pkg.el" "escreen.el") (20523
+;;;;;;  25781 999000))
+
+;;;***
+
+(provide 'escreen-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; escreen-autoloads.el ends here
---
+++ elpa/etags-table-20090327/etags-table-pkg.el
@@ -0,0 +1,1 @@
+(define-package "etags-table" "20090327" "Set tags table(s) based on current file [source: wiki]" (quote nil))
