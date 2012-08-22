a7cf8024bdca32fd290461a71e3135f55de95ac0
Zhao Vincent <v.zhao@sap.com>
2012-08-22 10:49:54 +0800

    use helm-git in emacs lisp mode

--- torment-helm.el
+++ torment-helm.el
@@ -1,5 +1,6 @@
 (require 'helm-config)
 (require 'helm-gtags)
+(require 'helm-git)
 
 (add-hook 'c-mode-common-hook
 		  (lambda ()
@@ -8,6 +9,11 @@
 (setq helm-c-gtags-path-style 'relative)
 (setq helm-c-gtags-ignore-case t)
 (setq helm-c-gtags-read-only t)
+
+(add-hook 'emacs-lisp-mode-hook
+		  (lambda ()
+			(global-set-key (kbd "M-O") 'helm-git-find-files)))
+
 
 (add-hook 'helm-gtags-mode-hook
 		  (lambda ()
