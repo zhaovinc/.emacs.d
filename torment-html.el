17bd9d73fe6dc948ccb14d767cd5ea7fe1858917
Zhao Vincent <ravensore@gmail.com>
2013-05-30 17:39:08 +1000

    add github package jquery-doc

--- torment-html.el
+++ torment-html.el
@@ -1,10 +1,12 @@
-(require 'impatient-mode)
 (require 'textile-mode)
 
 (add-to-list 'auto-mode-alist '("\\.textile\\'" . textile-mode))
 
 (if (equal system-type 'windows-nt)
 	(setq httpd-root "c:/work/sandbox/www"))
+
+(if (equal system-type 'darwin)
+    (setq httpd-root "~/Sandbox/html"))
 
 (add-hook 'html-mode-hook
           (lambda ()
--- torment-javascript.el
+++ torment-javascript.el
@@ -1,8 +1,11 @@
 (require 'js2-mode)
+(require 'jquery-doc)
 
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 
 (add-hook 'js2-mode-hook
 		  (lambda ()
 			(skewer-mode)
+			(jquery-doc-setup)
             (define-key js2-mode-map (kbd "M-j") 'backward-char)))
+
